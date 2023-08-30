7867384aa94f05e6b741de98907c588764d084bd
alexandr <KalX_si54@student.itstep.org>
2023-08-30 13:39:38 +0300

    Добавьте файлы проекта.

---
+++ Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_EF.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Entity.Department> Departments { get; set; }
+        public DbSet<Entity.Manager> managers { get; set; }
+
+        public DataContext() : base()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=ado-ef-p12;Integrated Security=True");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ADO_EF.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADO_EF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DataContext dataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataContext = new();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DepartmentsCountLabel.Content = dataContext.Departments.Count().ToString();
+            TopChiefCountLabel.Content = dataContext.managers.Where(manager => manager.IdChief == null).Count().ToString();
+            ChiefCountLabel.Content = dataContext.managers.Where(manager => manager.IdChief != null).Count().ToString();
+            MainDepCountLabel.Content = dataContext.managers.Where(manager => manager.IdMainDep != null).Count().ToString();
+            SecondDepCountLabel.Content = dataContext.managers.Where(manager => manager.IdSecDep != null).Count().ToString();
+            MainAndSecondDepCountLabel.Content = dataContext.managers.Where(manager => manager.IdMainDep != null && manager.IdSecDep != null).Count().ToString();
+        }
+    }
+}
